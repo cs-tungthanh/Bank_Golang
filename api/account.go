56662fd2390495e7a0fe27d23f72346eb691828a
Thanh Tung <tung@Thanhs-MacBook-Pro.local>
2022-06-12 11:28:56 +0700

    handle user sql

--- api/account.go
+++ api/account.go
@@ -6,6 +6,7 @@ import (
 
 	db "github.com/cs-tungthanh/Bank_Golang/db/sqlc"
 	"github.com/gin-gonic/gin"
+	pg "github.com/lib/pq"
 )
 
 type createAccountRequest struct {
@@ -28,6 +29,13 @@ func (server *Server) createAccount(ctx *gin.Context) {
 
 	account, err := server.store.CreateAccount(ctx, arg)
 	if err != nil {
+		if pgError, ok := err.(*pg.Error); ok {
+			switch pgError.Code.Name() {
+			case "unique_violation", "foreign_key_violation":
+				ctx.JSON(http.StatusForbidden, errorResponse(err))
+				return
+			}
+		}
 		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
 		return
 	}
